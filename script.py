d0175389d28dd3f965cf9d63817bb66630d17794
Kisekka David <dk@certainly.io>
2022-08-20 13:01:25 +0300

    refactor in code

--- script.py
+++ script.py
@@ -13,28 +13,16 @@ from spl.token.instructions import get_associated_token_address, create_associat
 from usdc_swaps import route_map
 
 
-# This is a script to perform arbitrage on the solana network using python
-
-solana_client = AsyncClient('https://api.mainnet-beta.solana.com')
-
-# Fetch your wallet using the secret key
-wallet = Keypair.from_secret_key(based58.b58decode('xxx'.encode("ascii")))
+SOLANA_CLIENT = AsyncClient('https://solana-mainnet.g.alchemy.com/v2/TFIFl5qbWkJcGklE_sKJWgpj3gPE6g32')
+WALLET = Keypair.from_secret_key(based58.b58decode('347D6oFeLYzPN1G5ZnFAMcTrezG1eVPfgb8L5eKKVVexHS1DhmD2v6Y76qXJ9YJpLk8PFSLiZt9EveZf2Qwdszhq'.encode("ascii")))
+INPUT_USDC_AMOUNT = 2000000
+USDC_BASE = 1000000
 
 
 def get_mint(index, indexedRouteMap):
    return indexedRouteMap['mintKeys'][int(index)]
 
 def get_route_map():
-    # You could do this
-
-    # generatedRouteMap = {}
-    # indexedRouteMap = requests.get('https://quote-api.jup.ag/v1/indexed-route-map').json()
-    # for key, index in indexedRouteMap['indexedRouteMap'].items():
-    #     generatedRouteMap[get_mint(key, indexedRouteMap)] = list(map(lambda item: get_mint(item, indexedRouteMap) ,indexedRouteMap['indexedRouteMap'][key]))
-    # print("the generatedRouteMap: ", generatedRouteMap['EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'])
-    # return generatedRouteMap
-
-    # or use the usdc_swaps.py file directly
     return route_map
     
 
@@ -52,7 +40,7 @@ async def get_coin_swap_quote(route):
            url='https://quote-api.jup.ag/v1/swap',
            json={
                 'route': route,
-                'userPublicKey': str(wallet.public_key),
+                'userPublicKey': str(WALLET.public_key),
                 'wrapUnwrapSOL': False
             },
             timeout=15.0
@@ -62,15 +50,12 @@ async def get_coin_swap_quote(route):
 
 async def execute_trancation(transacx):
     # Execute transactions
-    opts = TxOpts(skip_preflight=True)
+    opts = TxOpts(skip_preflight=True , max_retries=11)
     for tx_name, raw_transaction in transacx.items():
         if raw_transaction:
             try:
                 transaction = Transaction.deserialize(base64.b64decode(raw_transaction))
-                tx = await solana_client.send_transaction(transaction, wallet, opts=opts)
-                print("tx result: ", tx)
-                txid = tx.get("result")
-                print(f"transaction details :https://solscan.io/tx/{txid}")
+                await SOLANA_CLIENT.send_transaction(transaction, WALLET, opts=opts)
             except Exception as e:
                 print("Error occured at ex tx: ", str(e))
                 return str(e)
@@ -81,7 +66,7 @@ async def serialized_swap_transaction(usdcToTokenRoute, tokenToUsdcRoute):
             usdcToTokenTransaction = await get_coin_swap_quote(usdcToTokenRoute)
             await execute_trancation(usdcToTokenTransaction )
         except Exception as e:
-            print("Error occured at ser usdctotoken: ", str(e))
+            print("Error occured at execution usdctotoken: ", str(e))
             return str(e)
 
         if tokenToUsdcRoute:
@@ -89,41 +74,36 @@ async def serialized_swap_transaction(usdcToTokenRoute, tokenToUsdcRoute):
                 tokenToUsdcTransaction = await get_coin_swap_quote(tokenToUsdcRoute)
                 await execute_trancation(tokenToUsdcTransaction)
             except Exception as e:
-                print("Error occured at ser tokentousdc: ", str(e))
+                print("Error occured at execution tokentousdc: ", str(e))
                 return str(e)
 
 async def _create_associated_token_account(token):
     # Create Associated token account if not available
     token_associated_account = get_associated_token_address(
-        wallet.public_key,
+        WALLET.public_key,
         PublicKey(token)
     )
-    opts = TxOpts(skip_preflight=True)
-    ata = await solana_client.get_account_info(PublicKey(token_associated_account))
+    opts = TxOpts(skip_preflight=True , max_retries=11)
+    ata = await SOLANA_CLIENT.get_account_info(PublicKey(token_associated_account))
     if not ata.get('result').get('value'):
         try:
             instruction = create_associated_token_account(
-                wallet.public_key,
-                wallet.public_key,
+                WALLET.public_key,
+                WALLET.public_key,
                 PublicKey(token)
             )
             txn = Transaction().add(instruction)
-            txn.recent_blockhash = await solana_client.get_recent_blockhash()
-            resp = await solana_client.send_transaction(txn, wallet, opts=opts)
-            print("Response:", resp)
+            txn.recent_blockhash = await SOLANA_CLIENT.get_recent_blockhash()
+            await SOLANA_CLIENT.send_transaction(txn, WALLET, opts=opts)
         except Exception as e:
             print("Error occured while creating ata: ", str(e))
         
     else:
         print("Associated token account exists: ", ata)
 
-    return ata
 
 async def swap(input, generatedRouteMap):
-
     while True:
-        print('**********************************************************')
-        print('\n\n')
         for token in generatedRouteMap[:150]:
             usdcToToken = await get_coin_quote(
                 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
@@ -134,19 +114,19 @@ async def swap(input, generatedRouteMap):
                 tokenToUsdc = await get_coin_quote(
                     token,
                     'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
-                    usdcToToken.get('data')[0].get('outAmountWithSlippage')
+                    usdcToToken.get('data')[0].get('otherAmountThreshold')
                 )
 
                 if tokenToUsdc.get('data'):
-                    if tokenToUsdc.get('data')[0].get('outAmountWithSlippage') > input:
-                        print("=========> Bingo:", token, " || ", tokenToUsdc.get('data')[0].get('outAmountWithSlippage') / 1000000)
-                        ata_created = await _create_associated_token_account(token)
+                    if tokenToUsdc.get('data')[0].get('otherAmountThreshold') > input:
+                        print("=========> Bingo:", token, " || ", tokenToUsdc.get('data')[0].get('otherAmountThreshold') / USDC_BASE)
+                        await _create_associated_token_account(token)
                         await serialized_swap_transaction(usdcToToken.get('data')[0], tokenToUsdc.get('data')[0])
-                        profit = tokenToUsdc.get('data')[0].get('outAmountWithSlippage') - input
-                        print("Approx Profit made: ", profit / 1000000)
+                        profit = tokenToUsdc.get('data')[0].get('otherAmountThreshold') - input
+                        print("Approx Profit made: ", profit / USDC_BASE)
+                        print("\n")
     
 
 if __name__ == '__main__':
     generatedRouteMap = get_route_map()
-    # Swap 5 USDC
-    asyncio.run(swap(5000000, generatedRouteMap))
+    asyncio.run(swap(INPUT_USDC_AMOUNT, generatedRouteMap))
